f26823f3a3e2f02edf5121c145c7d38483a78a81
Erik Hanson <erik@eahanson.com>
2018-03-23 10:02:29 -0700

    add to enum

--- lib/corex/extra/enum.ex
+++ lib/corex/extra/enum.ex
@@ -8,4 +8,16 @@ defmodule Corex.Extra.Enum do
   def compact_blank(enum) do
     enum |> compact |> Enum.reject(&(Presence.is_blank?(&1)))
   end
+
+  def at!(enum, index) do
+    if length(enum) < (index + 1) do
+      raise "Out of range: index #{index} of enum with length #{length(enum)}: #{inspect(enum)}"
+    else
+      Enum.at(enum, index)
+    end
+  end
+
+  def tids(enumerable) do
+    enumerable |> Enum.map(fn (e) -> e.tid end)
+  end
 end
