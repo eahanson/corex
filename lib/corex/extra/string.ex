17c164a2aa81f6da345eae3320c63f443e2677a7
Erik Hanson <erik@eahanson.com>
2018-03-23 10:15:28 -0700

    String.surround and .inner_truncate

--- lib/corex/extra/string.ex
+++ lib/corex/extra/string.ex
@@ -1,5 +1,17 @@
 defmodule Corex.Extra.String do
+  def surround(s, surrounder) do
+    surround(s, surrounder, surrounder)
+  end
+
   def surround(s, beginning, ending) do
     beginning <> s <> ending
+  end
+
+  def inner_truncate(s, length) do
+    if String.length(s) <= length do
+      s
+    else
+      "#{String.slice(s, 0..9)}…#{String.slice(s, -10..-1)}"
+    end
   end
 end
---
+++ test/corex/extra/string_test.exs
@@ -0,0 +1,22 @@
+defmodule Corex.Extra.StringTest do
+  use Corex.SimpleCase, async: true
+
+  alias Corex.Extra
+
+  describe "inner_truncate" do
+    test "truncates from the middle of the string" do
+      assert Extra.String.inner_truncate("http://short.jpg", 20) == "http://short.jpg"
+      assert Extra.String.inner_truncate("http://www.example.com/apple/banana/cherry/donut/eclair.jpg", 20) == "http://www…eclair.jpg"
+    end
+  end
+
+  describe "surround" do
+    test "surrounds with some characters" do
+      assert Extra.String.surround("abc", "12") == "12abc12"
+    end
+
+    test "surrounds with beginning and ending characters" do
+      assert Extra.String.surround("abc", "12", "34") == "12abc34"
+    end
+  end
+end
