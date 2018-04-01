cabf14af20ae8bd6109bba8bae4d2eda99b3a0f2
Erik Hanson <erik@eahanson.com>
2018-04-01 15:01:13 -0700

    data table

--- test/corex_web/controllers/user_controller_test.exs
+++ test/corex_web/controllers/user_controller_test.exs
@@ -6,7 +6,7 @@ defmodule CorexWeb.UserControllerTest do
   describe "index" do
     test "lists all users", %{conn: conn} do
       conn = get conn, user_path(conn, :index)
-      assert html_response(conn, 200) =~ "Listing Users"
+      assert html_response(conn, 200) =~ "Users"
     end
   end
 
