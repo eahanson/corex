63a09d0dd2b1032edf92c6300bbc9afc86dfcad8
Erik Hanson <erik@eahanson.com>
2018-07-07 19:09:20 -0400

    seeds creates admin and uesr

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,8 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Corex.Accounts
+
+%{email: "admin@example.com", password: "password123", admin: true} |> Accounts.create_admin()
+%{email: "user@example.com", password: "password123", admin: false} |> Accounts.create_user()
