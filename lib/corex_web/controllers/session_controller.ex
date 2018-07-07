6b9c7b1eeed389003cf9719e3ee6c19251be47f5
Erik Hanson <erik@eahanson.com>
2018-07-07 19:42:14 -0400

    SessionController checks user role

--- lib/corex_web/controllers/session_controller.ex
+++ lib/corex_web/controllers/session_controller.ex
@@ -1,16 +1,22 @@
 defmodule CorexWeb.SessionController do
   use CorexWeb, :controller
 
+  import CorexWeb.AuthGuards
+
   alias Corex.Accounts
   alias Corex.Accounts.User
   alias CorexWeb.Session
 
-  def new(conn, _params) do
+  def action(conn, _) do
+    apply(__MODULE__, action_name(conn), [conn, conn.params, Session.get_current_user_role(conn)])
+  end
+
+  def new(conn, _params, _role) do
     changeset = Accounts.change_user(%User{})
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"user" => %{"email" => email, "password" => password}}) do
+  def create(conn, %{"user" => %{"email" => email, "password" => password}}, _role) do
     case Accounts.get_user(email: email, password: password) do
       {:ok, user} ->
         conn
@@ -23,7 +29,7 @@ defmodule CorexWeb.SessionController do
     end
   end
 
-  def delete(conn, _params) do
+  def delete(conn, _params, role) when is_member?(role) do
     conn
     |> Plug.Conn.clear_session()
     |> redirect(to: landing_path(conn, :index))
--- test/corex_web/controllers/session_controller_test.exs
+++ test/corex_web/controllers/session_controller_test.exs
@@ -2,22 +2,36 @@ defmodule CorexWeb.SessionControllerTest do
   use CorexWeb.ConnCase
   use Plug.Test
 
+  import Corex.Test.Auth, only: [assert_auth: 3]
+
   alias Corex.Accounts
   alias Corex.Test
   alias CorexWeb.Endpoint
   alias CorexWeb.Session
 
   describe "new session" do
+    defp get_new(conn) do
+      get(conn, session_path(conn, :new))
+    end
+
     test "renders form", %{conn: conn} do
-      conn = get(conn, session_path(conn, :new))
+      conn = get_new(conn)
       assert html_response(conn, 200) =~ "Log In"
+    end
+
+    test "allows guest+", %{conn: conn} do
+      conn |> assert_auth(:guest, &get_new/1)
     end
   end
 
   describe "create session" do
+    defp post_create(conn, opts \\ []) do
+      post(conn, session_path(conn, :create), user: Test.Mom.user_attrs("test", opts))
+    end
+
     test "logs user in when credentials are valid", %{conn: conn} do
       {:ok, user} = Test.Mom.user_attrs("test") |> Accounts.create_user()
-      conn = post(conn, session_path(conn, :create), user: %{email: "test@example.com", password: "password123"})
+      conn = post_create(conn)
 
       assert redirected_to(conn) == profile_path(conn, :show)
 
@@ -28,19 +42,31 @@ defmodule CorexWeb.SessionControllerTest do
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
-      conn = post(conn, session_path(conn, :create), user: Test.Mom.user_attrs("user") |> Map.put(:email, "bad"))
+      conn = post_create(conn, email: "bad")
       assert html_response(conn, 200) =~ "Log In"
+    end
+
+    test "allows guest+", %{conn: conn} do
+      conn |> assert_auth(:guest, fn conn -> post_create(conn) end)
     end
   end
 
   describe "delete session" do
+    defp delete_delete(conn) do
+      delete(conn, session_path(Endpoint, :delete))
+    end
+
     test "logs user out", %{conn: conn} do
       {:ok, user} = Test.Mom.user_attrs("test") |> Accounts.create_user()
       conn = conn |> Test.Session.login(user)
 
-      conn = delete(conn, session_path(Endpoint, :delete))
+      conn = delete_delete(conn)
       assert redirected_to(conn) == landing_path(Endpoint, :index)
       assert conn |> Session.get_current_user_id() == nil
+    end
+
+    test "requires member+", %{conn: conn} do
+      conn |> assert_auth(:member, &delete_delete/1)
     end
   end
 end
