414c716ad124471075e6aba4418dc9d81cb6553f
Erik Hanson <erik@eahanson.com>
2018-07-07 19:57:27 -0400

    LandingController checks user role

--- lib/corex_web/controllers/landing_controller.ex
+++ lib/corex_web/controllers/landing_controller.ex
@@ -1,7 +1,13 @@
 defmodule CorexWeb.LandingController do
   use CorexWeb, :controller
 
-  def index(conn, _params) do
-    render conn, "index.html", hide_nav: true
+  alias CorexWeb.Session
+
+  def action(conn, _) do
+    apply(__MODULE__, action_name(conn), [conn, conn.params, Session.get_current_user_role(conn)])
+  end
+
+  def index(conn, _params, _role) do
+    render(conn, "index.html", hide_nav: true)
   end
 end
--- test/corex_web/controllers/landing_controller_test.exs
+++ test/corex_web/controllers/landing_controller_test.exs
@@ -1,8 +1,20 @@
 defmodule CorexWeb.LandingControllerTest do
   use CorexWeb.ConnCase, async: true
 
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Corex"
+  import Corex.Test.Auth, only: [assert_auth: 3]
+
+  describe "get index" do
+    defp get_index(conn) do
+      get(conn, "/")
+    end
+
+    test "GET /", %{conn: conn} do
+      conn = get_index(conn)
+      assert html_response(conn, 200) =~ "Corex"
+    end
+
+    test "allows guest+", %{conn: conn} do
+      conn |> assert_auth(:guest, &get_index/1)
+    end
   end
 end
